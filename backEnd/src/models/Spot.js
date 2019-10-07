a1b0cca2382fea8eacfee15da774cc91822c4708
Luka tassano <lukatassano070@gmail.com>
2019-10-06 23:37:07 -0300

    2.0

--- backEnd/src/models/Spot.js
+++ backEnd/src/models/Spot.js
@@ -6,9 +6,17 @@ const SpotSchema = new mongoose.Schema({
   price: Number,
   techs: [String],
   user: {
-    type: mongoose.Schema.ObjectId,
+    type: mongoose.Schema.Types.ObjectId,
     ref: 'User'
   }
+}, {
+  toJSON: {
+    virtuals: true,
+  }
 });
+
+SpotSchema.virtual('thumbnail_url').get(function() {
+  return `http://localhost:3333/files/${this.thumbnail}`
+})
 
 module.exports = mongoose.model('Spot', SpotSchema);
